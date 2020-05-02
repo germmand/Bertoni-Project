a1737785600d6cfd4a12bf78d27312245be4e3a6
germmand <german.aguilera17@gmail.com>
2020-05-02 13:56:16 -0400

    refactor: Clean up the layout a little bit.

--- Bertoni.Web/Controllers/HomeController.cs
+++ Bertoni.Web/Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace Bertoni.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
